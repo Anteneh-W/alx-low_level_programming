f52af5095a3502ef430b06bbbf4e27fcd1496b31
Anteneh-W <antenehwondale@gmail.com>
2022-04-09 03:49:18 +0300

    tasks 0 - 4

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * This program assigns a random value to variable n
+ * it prints the last digit of n is greater than 5, less than 6, 0r 0
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - Entry Point
+ *  This program prints the alphabet in lowercasethen in uppercase
+ *  Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; h <= 'Z' ; h++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
